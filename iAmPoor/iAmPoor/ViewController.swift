f523b2b6ced2c03138126f907ece2b912d9a6e9d
tamioTech <62820237+tamioTech@users.noreply.github.com>
2020-03-29 19:51:05 -0700

    Add files via upload

---
+++ iAmPoor/iAmPoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAmPoor
+//
+//  Created by Tyler Hikida on 2020-03-27.
+//  Copyright © 2020 tamioTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
